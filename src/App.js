9408d9bcd939788262bb7e07d238b8193361047c
Hari Krishna Gadi <gadiharikrishna@gmail.com>
2023-07-13 22:50:26 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    Events.scrollEvent.register('begin', () => {
+      console.log('scrolling has started');
+    });
+
+    Events.scrollEvent.register('end', () => {
+      console.log('scrolling has ended');
+    });
+
+    scrollSpy.update();
+
+    return () => {
+      Events.scrollEvent.remove('begin');
+      Events.scrollEvent.remove('end');
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link
+              activeClass="active"
+              to="section1"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Intro
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="section2"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Section 2
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="section3"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Section 3
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Element name="section1" className="element">
+        <h1>Konsumräume <br/>retten <br/>leben</h1>
+      </Element>
+
+      <Element name="section2" className="element">
+        <p>Die Zahl der Drogentoten ist im letzten Jahr in München erneut dramatisch angestiegen. Schon lange ist klar: Konsumräume wie sie in anderen Bundesländern bereits bestehen retten leben! 
+
+In Bayern gibt es aktuell keine Konsumräume bisher nicht, obwohl die Stadt diese bereits seit langem fordert. 2010 und 2018 gab es entsprechende Stadtratsbeschlüsse. (https://www.muenchen-transparent.de/antraege/5017665)
+
+Auch andere Kommunen in Bayern haben sich immer wieder für Konsumräume eingesetzt. 
+
+Die nötige Verordnung wurde bei der bayerischen Staatsregierung erbeten. Diese entschied sich aber trotz positiver Erfahrungswerte dagegen. 
+
+wir wollen uns erneut für Drogenkonsumräume in Bayern stark machen und dafür als breites Bündnis eine Petition im bayrischen Landtag einreichen.</p>
+      </Element>
+
+      <Element name="section3" className="element">
+        <h1>Section 3</h1>
+      </Element>
+
+      <button className="scroll-top-button" onClick={scrollToTop}>
+        Scroll to Top
+      </button>
     </div>
   );
-}
+};
 
 export default App;
